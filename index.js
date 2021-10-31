ddbdacb5ddc6e2bfa9a02e58665526b3f8d42e41
Vahid Mohammadi <vahidm1467@gmail.com>
2021-10-31 13:53:29 +0330

    perf: update to stylelint 14.0.1 and add severity to rules

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: ['stylelint-config-standard-scss'],
   plugins: [
     'stylelint-declaration-strict-value',
     'stylelint-no-unused-selectors',
@@ -9,25 +9,31 @@ module.exports = {
     '@signal-noise/stylelint-scales',
   ],
   rules: {
-    'selector-class-pattern': '^([a-z1-9]+-)*([a-z1-9]+)$',
-    'selector-type-case': 'lower',
-    'property-case': 'lower',
-    'block-no-empty': true,
-    'color-no-invalid-hex': true,
-    'comment-no-empty': true,
-    'no-empty-source': null,
+    'selector-class-pattern': ['^([a-z1-9]+-)*([a-z1-9]+)$', { severity: 'warning' }],
+    'selector-type-case': ['lower', { severity: 'error' }],
+    'property-case': ['lower', { severity: 'error' }],
+    'unit-case': ['lower', { severity: 'error' }],
     'rule-empty-line-before': [
       'always-multi-line',
       {
         except: ['first-nested'],
         ignore: ['after-comment'],
+        severity: 'error',
       },
     ],
-    'color-named': 'never',
-    'at-rule-disallowed-list': [['extend', 'debug']],
+
+    'block-no-empty': [true, { severity: 'error' }],
+    'comment-no-empty': [true, { severity: 'error' }],
+    'no-empty-source': null,
+
+    'color-no-invalid-hex': [true, { severity: 'error' }],
+    'color-named': ['never', { severity: 'error' }],
+    'color-hex-length': null,
+
+    'at-rule-disallowed-list': [['extend', 'debug'], { severity: 'error' }],
     'at-rule-no-unknown': null,
+
     'no-invalid-position-at-import-rule': null,
-    'unit-case': 'lower',
     'scale-unlimited/declaration-strict-value': [
       ['/color$/', 'z-index'],
       {
@@ -43,14 +49,15 @@ module.exports = {
         message:
           'Avoid using magical values like "${value}" for "${property}". Instead, use predefined variables like `var(--primary)`',
         disableFix: true,
+        severity: 'warning',
       },
     ],
-    'plugin/no-unused-selectors': [true],
-    'selector-max-compound-selectors': 4,
-    'no-duplicate-selectors': [true],
-    'color-hex-length': null,
+    'plugin/no-unused-selectors': [true, { severity: 'warning' }],
+    'selector-max-compound-selectors': [4, { severity: 'warning' }],
+    'no-duplicate-selectors': [true, { severity: 'error' }],
     'plugin/selector-tag-no-without-class': [
-      ['div', 'span', '/h\\d/', 'section', 'p', 'ul', 'table', 'li', 'tbody', 'th', 'td'],
+      ['div', 'span', '/h\\d/', 'section', 'p', 'ul', 'table', 'li', 'tbody', 'th', 'td', 'img'],
+      { severity: 'error' },
     ],
     'liberty/use-logical-spec': [
       true,
@@ -66,28 +73,40 @@ module.exports = {
           'left',
           'right',
         ],
+        severity: 'error',
       },
     ],
-    'pitcher/max-lines': 500,
-    'scales/border-widths': [{ scale: [1, 2], units: ['px'] }],
-    'scales/font-families': ['system-ui', 'monospace', 'dana', 'sans-serif'],
+    'pitcher/max-lines': [500, { severity: 'error' }],
+    'scales/border-widths': [
+      [{ scale: [1, 2], units: ['px'] }],
+      { severity: 'warning', disableFix: true },
+    ],
+    'scales/font-families': [
+      ['system-ui', 'monospace', 'dana', 'sans-serif'],
+      { severity: 'error', disableFix: true },
+    ],
     'scales/font-sizes': [
-      { scale: [0.75, 0.875, 1, 1.125, 1.25, 1.5, 2], units: ['rem'], disableFix: true },
-      // { scale: [12, 14, 16, 18, 20, 24], units: ['px'], disableFix: true },
+      [{ scale: [0.75, 0.875, 1, 1.125, 1.25, 1.5, 2], units: ['rem'] }],
+      { severity: 'error', disableFix: true },
     ],
     // 'scales/font-weights': ['normal', 'bold'], // doesn't support non-numeric values
     // 'scales/line-heights': [1, 1.5],
     // 'scales/z-indices': [-1,0, 1, 999],
     'scales/radii': [
-      { scale: [0, 4, 10, 25, 50], units: ['px'], disableFix: true },
-      { scale: [50], units: ['%'], disableFix: true },
+      [
+        { scale: [0, 4, 10, 25, 50], units: ['px'] },
+        { scale: [50], units: ['%'] },
+      ],
+      { severity: 'warning', disableFix: true },
     ],
     'scales/space': [
-      {
-        scale: [0.125, 0.25, 0.375, 0.5, 0.75, 1, 1.5, 2, 2.5, 3],
-        units: ['rem'],
-        disableFix: true,
-      },
+      [
+        {
+          scale: [0.125, 0.25, 0.375, 0.5, 0.75, 1, 1.5, 2, 2.5, 3],
+          units: ['rem'],
+        },
+      ],
+      { severity: 'error', disableFix: true },
     ],
     'declaration-property-unit-allowed-list': [
       {
@@ -95,9 +114,10 @@ module.exports = {
         '/^(padding|margin)/': ['rem', 'vh', 'vw', '%'],
         '/^border-.*radius$/': ['px', '%'],
       },
+      { severity: 'warning' },
     ],
   },
   reportNeedlessDisables: [true],
   reportDescriptionlessDisables: [true],
-  // defaultSeverity: 'warning',
+  defaultSeverity: 'warning',
 };
